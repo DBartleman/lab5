26f14f4e200850e2132634053b14ee2ac377c8f2
Andrew Bartleman <abartlem@ucsd.edu>
2017-02-09 14:54:51 -0800

    added friends

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,15 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
+	var name = req.query.name;
+	var description = req.query.description;
+
+	var newfriend = {
+		name: name, 
+		description: description, 
+		imageURL: "#"
+	}
+
+	data.friends.push(newfriend);
+	res.redirect('/');
  }
